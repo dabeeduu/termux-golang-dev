d0b1c311e9e74105ab80a548243d9ccc039c2a96
dabeeduu <dabeeduu+git@gmail.com>
2025-04-02 22:55:17 +0700

    Update setup_script.sh

--- setup_script.sh
+++ setup_script.sh
@@ -18,40 +18,61 @@ if [ "$password" != "$password_confirm" ]; then
 fi
 
 # Update and Upgrade Termux
-pkg update -y
-pkg upgrade -y
+#pkg update -y
+#pkg upgrade -y
 
 # Install X11 repositories and tools
-pkg install x11-repo -y
-pkg install termux-x11-nightly -y
-pkg install xfce -y
-pkg install proot-distro -y
+#pkg install x11-repo -y
+#pkg install termux-x11-nightly -y
+#pkg install xfce -y
+#pkg install proot-distro -y
 
 # Install and configure Ubuntu in Termux
-pkg install proot-distro -y
-proot-distro install ubuntu -y
+#pkg install proot-distro -y
+#proot-distro install ubuntu
 
 # Log into Ubuntu
-proot-distro login ubuntu
+# proot-distro login ubuntu
+
+
+proot-distro login ubuntu bash -c "
+  # Inside Ubuntu - update and upgrade
+  apt update -y
+  apt upgrade -y
+
+  # Install sudo
+  apt install sudo -y
+
+  # Add the user and set the password
+  adduser $username <<EOF
+  $password
+  $password
+  EOF
+
+  # Add user to sudoers
+  echo \"$username  ALL=(ALL) NOPASSWD: ALL\" >> /etc/sudoers
+
+  exit
+"
 
 # Inside Ubuntu - update and upgrade
-apt update -y
-apt upgrade -y
+#apt update -y
+#apt upgrade -y
 
 # Install sudo
-apt install sudo -y
+#apt install sudo -y
 
 # Add the user and set the password
-adduser $username <<EOF
-$password
-$password
-EOF
+#adduser $username <<EOF
+#$password
+#$password
+#EOF
 
 # Edit sudoers file to give new user sudo privileges
-echo "$username ALL=(ALL:ALL) ALL" >> /etc/sudoers
+#echo "$username ALL=(ALL:ALL) ALL" >> /etc/sudoers
 
 # Exit back to Termux
-exit
+#exit
 
 # Download a login script from a remote URL
-wget -O login_script.sh https://www.test.com
+#wget -O login_script.sh https://www.test.com
